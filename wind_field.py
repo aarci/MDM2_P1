bdc31211d3a29d17760a42a77c1b5c1c721ea8b8
bm7273 <ut22994@bristol.ac.uk>
2024-11-01 17:39:22 +0000

    added ability to create uniform wind fields

--- wind_field.py
+++ wind_field.py
@@ -4,16 +4,32 @@ from perlin_numpy import (
     generate_perlin_noise_2d
 )
 
-def generate_wind_field():
+def generate_wind_field(shape = (1000, 1000)):
 
     '''
-    Outputs X and Y components of the 2D vector field corresponding to the wind
+    Outputs X and Y components of the random 2D vector field corresponding to the wind
     '''
 
     np.random.seed()
 
-    angles = (2 * np.pi) * generate_perlin_noise_2d((1000, 1000), (8,8))
-    mags = 100 * (0.1 + generate_perlin_noise_2d((1000, 1000), (8,8)))
+    angles = (2 * np.pi) * generate_perlin_noise_2d(shape, (8,8))
+    mags = 10 * (0.1 + generate_perlin_noise_2d(shape, (8,8)))
+
+    X = mags * np.cos(angles)
+    Y = mags * np.sin(angles)
+
+    return X, Y
+
+def generate_wind_field_uniform(angle, shape = (1000, 1000)):
+
+    '''
+    Outputs X and Y components of the random 2D vector field corresponding to the wind
+    '''
+
+    np.random.seed()
+
+    angles = np.full(shape, angle)
+    mags = np.full(shape, 2)
 
     X = mags * np.cos(angles)
     Y = mags * np.sin(angles)
